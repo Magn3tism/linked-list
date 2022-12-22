04721a71ff494d0b2837ea3c53b1012daed4beeb
magn3tism <chapagainsakshyam@gmail.com>
2022-12-22 11:45:25 +0545

    Add all methods

--- linkedList.js
+++ linkedList.js
@@ -12,52 +12,105 @@ class LinkedList {
   }
 
   append(value) {
-    this.length++;
+    this.#length++;
     const newNode = new Node(value);
 
-    if (this.HEAD === null) this.HEAD = newNode;
-    if (this.HEAD !== newNode) this.TAIL.next = newNode;
+    if (this.#HEAD === null) this.#HEAD = newNode;
+    if (this.#HEAD !== newNode) this.#TAIL.next = newNode;
 
-    if (newNode.next === null) this.TAIL = newNode;
+    if (newNode.next === null) this.#TAIL = newNode;
   }
 
   prepend(value) {
-    this.length++;
+    this.#length++;
     const newNode = new Node(value);
 
-    if (this.HEAD) {
-      newNode.next = this.HEAD;
-      // this.TAIL = newNode.next;
+    if (this.#HEAD) newNode.next = this.#HEAD;
+
+    this.#HEAD = newNode;
+
+    if (!newNode.next) this.#TAIL = newNode;
+    else this.#TAIL = newNode.next;
+  }
+
+  get size() {
+    return this.#length;
+  }
+
+  get getHead() {
+    return this.#HEAD;
+  }
+
+  get getTail() {
+    return this.#TAIL;
+  }
+
+  at(index) {
+    let reqd = this.#HEAD;
+    for (let i = 0; i < index; i++) {
+      reqd = reqd.next;
     }
 
-    this.HEAD = newNode;
+    return reqd.value;
+  }
 
-    if (!newNode.next) this.TAIL = newNode;
-    else this.TAIL = newNode.next;
+  pop() {
+    let reqd = this.#HEAD;
+    for (let i = 0; i < this.#length - 1; i++) {
+      if (i === this.#length - 2) {
+        reqd.next = null;
+        this.#TAIL = reqd;
+      }
+      reqd = reqd.next;
+    }
+  }
+
+  contains(value) {
+    let reqd = this.#HEAD;
+    for (let i = 0; i < this.#length; i++) {
+      if (reqd.value === value) return true;
+      reqd = reqd.next;
+    }
+
+    return false;
+  }
+
+  find(value) {
+    let reqd = this.#HEAD;
+    for (let i = 0; i < this.#length; i++) {
+      if (reqd.value === value) return i;
+      reqd = reqd.next;
+    }
+
+    return null;
+  }
+
+  get toString() {
+    let reqd = this.#HEAD;
+    let string = "";
+
+    for (let i = 0; i <= this.#length; i++) {
+      if (i === this.#length) {
+        string += `null`;
+      } else {
+        string += `${reqd.value} -> `;
+        reqd = reqd.next;
+      }
+    }
+
+    return string;
   }
 }
 
 const link = new LinkedList();
 
-link.append("Hello");
-// console.log(link);
-
-// link.prepend("Hi!");
-// console.log(link);
-
-link.append("Unix!");
-// console.log(link);
-
 link.append("Unix1!");
-// console.log(link);
-
 link.append("Unix2!");
-// console.log(link);
-
 link.append("Unix3!");
-
 link.append("Unix4!");
+link.append("Unix5!");
+link.append("Unix6!");
+link.append("Unix7!");
+link.append("Unix8!");
 
-console.log(link.length);
-link.length = 8;
-console.log(link.length);
+console.log(link.toString);
